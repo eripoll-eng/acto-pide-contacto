b7579c69d8a19d5bdfa8104a86e339f270b0b8f1
estsewin <estebanestse@gmail.com>
2026-03-04 14:29:50 +0100

    feat: add express server

---
+++ app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Acto pide contacto Service');
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
